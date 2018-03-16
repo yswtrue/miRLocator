13cb4311d9544230ce9f95e250f3e23d2d2d3fdc
杨胜文 <yswtrue@yangshegwensMBP.lan>
2018-03-17 00:36:41 +0800

    first init

---
+++ lib.py
@@ -0,0 +1,135 @@
+# -*- coding: utf-8 -*-
+import os, sys
+import source
+
+#end
+
+##################################################################################################
+##############################parameters section##################################################
+##################################################################################################
+##the directory of RNAfold                                                                     ###
+RNAFoldDic = "/usr/bin/"  ###
+###
+##file directory of source.py                                                                  ###
+# sourceDic = "/home/cma/Researches/matureMiRNA/miRLocator/"  ###
+sourceDic = os.path.join(os.getcwd()) + '/'  ###
+###
+##result directory, include trainDataFileName, predDataFileName, predDataAnnotFileName         ###
+resultDic = sourceDic + "results/"  ###
+###
+###
+##a logical parameter indicate whether run cross validation on training dataset
+cross_validation_flag = False  ###
+###
+##miRNAs and pre-miRNAs for training                                                           ###
+trainDataFileName = "trainingData.txt"  ###
+##pre-miRNAs for prediction                                                                    ###
+predDataFileName = "predictionData.txt"  ###
+##the annotated miRNAs and pre-miRNAs for prediction dataset                                   ###
+###
+###
+##################################optional parameters ############################################
+##the full path of trained prediction model. If a full path is given,                          ###
+##miRLocator will locate it directly, otherwise, miRLocator will run the training program.     ###
+predModelFileDir = ""  ###
+##If the file name is defined, miRLocator will evaluate the prediction results                 ###
+##based on annotation infomation in this file                                                  ###
+predDataAnnotFileName = "predictionData_Annotated.txt"  ###
+##################################################################################################
+
+#####################################################################################################################################
+#####################################################################################################################################
+#keep this command, temp file used to record temporary results
+tempFileDir = resultDic + "tempResult.txt"
+
+predResultFileDir = resultDic + "miRLocator_predResults.txt"
+evalResultFileDir = resultDic + "miRLocator_evalResults.txt"
+
+#create file directories for recording dp_ss files for training and prediction
+dpSSFileDic_train = resultDic + "dp_ss_train/"
+dpSSFileDic_pred = resultDic + "dp_ss_pred/"
+
+
+def start(training_data_path, prediction_data_path,
+          prediction_data_annotated_path):
+    """开始执行任务"""
+    predModelFileDir = ""  ###
+
+    source.createDict(dpSSFileDic_train)
+    source.createDict(dpSSFileDic_pred)
+
+    trainDataFileDir_Ref = source.checkFileForTraining(
+        training_data_path,
+        RNAFoldDic,
+        sourceDic,
+        dpSSFileDic_train,
+        dpSSFlag=True)
+
+    ##cross_validation_test
+    if (cross_validation_flag == True):
+        source.cross_validation_function(
+            source.cv, trainDataFileDir_Ref, source.minCandidateMiRNALen,
+            source.maxCandidateMiRNALen, source.upOffSet, source.downOffSet,
+            source.minPredScore, resultDic, dpSSFileDic_train, RNAFoldDic,
+            tempFileDir)
+    #end if
+
+    ##train prediction model on all miRNAs in input file
+    ##trainDataFileDir_Ref: refined data for training
+    ##dpSSFileDic_train: dp_ss files for training data
+    ##RNAFoldDic: the directory of RNAfold
+    ##tempFileDir: tempoary file
+    ##source.upOffSet: 5 in default
+    ##source.downOffSet: 5 in default
+    #predModelFileDir: the full path of trained prediction model
+    ##resultDic: result directory
+    predModel = ""
+    if (len(predModelFileDir) == 0):
+        predModelFileDir = resultDic + "trained_prediction_model"
+        predModel = source.train_prediction_model(
+            trainDataFileDir_Ref, dpSSFileDic_train, RNAFoldDic, tempFileDir,
+            source.upOffSet, source.downOffSet, predModelFileDir, resultDic)
+    else:
+        predModel = source.loadPredModel(predModelFileDir)
+    #end else
+
+    #prediction mature miRNAs within pre-miRNA sequences
+    ##predDataFileDir_Ref:refined data for prediction
+    ##dpSSFileDic_pred: dp_ss files for prediction
+    ##RNAFoldDic:the directory of RNAfold
+    ##tempFileDir:tempoary file
+    ##source.minCandidateMiRNALen: 16 in default
+    ##source.maxCandidateMiRNALen: 30 in default
+    ##source.upOffSet: 5 in default
+    ##source.downOffSet: 5 in default
+    ##source.minPredScore: 0.25 in default, but not used in this version
+    ##predModel: trained prediction model
+    ##predResultFileDir: the full path of prediction results
+    predDataFileDir_Ref = ""
+    if (len(predDataFileName) > 0):
+        predDataFileDir_Ref = source.checkFileForPrediction(
+            prediction_data_path,
+            RNAFoldDic,
+            sourceDic,
+            dpSSFileDic_pred,
+            dpSSFlag=True)
+        source.prediction(predDataFileDir_Ref, dpSSFileDic_pred, RNAFoldDic,
+                          tempFileDir, source.minCandidateMiRNALen,
+                          source.maxCandidateMiRNALen, source.upOffSet,
+                          source.downOffSet, source.minPredScore, predModel,
+                          predResultFileDir)
+
+    #evaluate the prediction results
+    ##predDataAnnotFileDir: the annotation of pre-miRNAs used for prediction. For each pre-miRNA, the annotation information include: miRNA_ID, pre-miRNA_ID, miRNA_seq, pre-miRNA_seq, pre-miRNA_structure
+    ##predResultFileDir: the full path of predicted results output by miRLocator
+    ##evalResultFileDir: the full path of evaluation result
+    if (len(predDataAnnotFileName) > 0):
+        source.eval_dif_resolutions(prediction_data_annotated_path,
+                                    predResultFileDir, evalResultFileDir)
+
+
+def zipdir(path, ziph):
+    # ziph is zipfile handle
+    for root, dirs, files in os.walk(path):
+        for file in files:
+            ziph.write(os.path.join(root, file))
---
+++ main.py
@@ -0,0 +1,70 @@
+#!env python
+# -*- coding: utf-8 -*-
+from flask import Flask, render_template, request, jsonify, Response, json, send_from_directory
+import source
+import lib
+import os, sys
+import datetime
+import zipfile
+
+UPLOAD_FOLDER = os.path.join(os.getcwd(), 'upload')
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        # now_datetime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        # upload_path = os.path.join(app.config['UPLOAD_FOLDER'], now_datetime)
+        upload_path = app.config['UPLOAD_FOLDER']
+        if not os.path.exists(upload_path):
+            os.makedirs(upload_path)
+        # 判断文件
+        if 'trainingData' not in request.files:
+            raise Exception('没有trainingData.txt')
+            # return Response(json.dumps({'code': 500, 'msg': '没有选择文件'}), 500)
+        # 获取上传的文件
+        training_data = request.files['trainingData']
+
+        prediction_data = request.files.get('predictionData', None)
+        prediction_data_annotated = request.files.get(
+            'predictionData_Annotated', None)
+
+        training_data_path = os.path.join(upload_path, training_data.filename)
+        training_data.save(training_data_path)
+        # 判断是否有predictionData文件
+        prediction_data_path = os.path.join(upload_path, 'predictionData.txt')
+        if prediction_data:
+            prediction_data_path = os.path.join(upload_path,
+                                                prediction_data.filename)
+            prediction_data.save(prediction_data_path)
+        if not os.path.exists(prediction_data_path):
+            raise Exception('没有predictionData.txt')
+
+        # 判断是否有predictionData_Annotated文件
+        prediction_data_annotated_path = os.path.join(
+            upload_path, 'predictionData_Annotated.txt')
+        if prediction_data_annotated:
+            prediction_data_annotated_path = os.path.join(
+                upload_path, prediction_data_annotated.filename)
+            prediction_data_annotated.save(prediction_data_annotated_path)
+        if not os.path.exists(prediction_data_annotated_path):
+            raise Exception('没有predictionData_Annotated.txt')
+        lib.start(training_data_path, prediction_data_path,
+                  prediction_data_annotated_path)
+        zipFile = zipfile.ZipFile(
+            os.path.join(upload_path, 'results.zip'), 'w',
+            zipfile.ZIP_DEFLATED)
+        lib.zipdir(os.path.join(lib.sourceDic, 'results'), zipFile)
+        zipFile.close()
+        return send_from_directory(
+            directory=upload_path, filename='results.zip')
+    else:
+        # 显示首页
+        return render_template('index.html')
+
+
+if __name__ == '__main__':
+    app.run()
